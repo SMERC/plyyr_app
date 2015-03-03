7da38ee87a5dc0736684e92069c4b8370cefdd1e
maugarci <mauricio.garcia@towerhousestuido.com>
2015-03-03 15:08:51 -0200

    preparing for login

--- www/js/index.js
+++ www/js/index.js
@@ -37,7 +37,7 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        window.open("http://plyyr.com", "_self");
+        window.open("http://dev.plyyr.com", "_self");
     }
 };
 
